861b84c6c485a9df06b61775483421095447aeaf
makiavelli747 <57914634+makiavelli747@users.noreply.github.com>
2022-11-18 01:03:48 +0100

    Add files via upload

--- common/defines.lua
+++ common/defines.lua
@@ -695,10 +695,10 @@ pops = {
 ai =
 {
 	COLONY_WEIGHT = 10.0, -- ai weight for colonising
-	ADMINISTRATOR_WEIGHT = 10.0, -- ai weight for new bureaucrat
+	ADMINISTRATOR_WEIGHT = 30.0, -- ai weight for new bureaucrat
 	INDUSTRYWORKER_WEIGHT = 10.0, -- ai weight for new industry workers
 	EDUCATOR_WEIGHT = 30.0, -- ai weigth for new clergy
-	SOLDIER_WEIGHT = 30.0, -- ai weight for soldiers
+	SOLDIER_WEIGHT = 10.0, -- ai weight for soldiers
 	SOLDIER_FRACTION = 0.05, -- max amount of population AI wants to be soldiers
 	CAPITALIST_FRACTION = 0.001, -- max amount of population AI wants to be capis
 	PRODUCTION_WEIGHT = 10.0, -- ai weight for new production
